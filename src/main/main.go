77b147ed602904e098323cb11dc0c9b5b3c8dd4e
Egor Korabelnikov <korabelnikovegor@mail.ru>
2019-10-13 21:40:08 +0300

    init

--- src/main/main.go
+++ src/main/main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+
+	connect := os.Getenv("DATABASE_URL")
+	if connect == "" {
+		connect = "dbname=" + "klynxe" + " user=" + "egor" +
+			" password=" + "12345" + " host=" + "localhost" + " port=" + "5432" + " sslmode=" + "disable"
+	}
+	fmt.Println("%v", t.T)
+	_, err := sql.Open("postgres", connect)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
